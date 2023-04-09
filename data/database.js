20de6736f2450dfeacd24341de603af265660038
sukhendup <sukhendupatra007@gmail.com>
2023-04-10 01:37:27 +0530

    database modified

--- data/database.js
+++ data/database.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
